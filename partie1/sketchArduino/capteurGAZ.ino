8af7d4cd261690a3a2ea0af47e2792cd97811456
Birahima FALL <birahimafall@gmail.com>
2014-04-11 11:56:33 +0200

    premier commit

---
+++ partie1/sketchArduino/capteurGAZ.ino
@@ -0,0 +1,14 @@
+int sensorValue;
+
+void setup()
+{
+  Serial.begin(9600);      // sets the serial port to 9600
+}
+
+void loop()
+{
+  analogReadResolution(10);
+  sensorValue = analogRead(A0);       // read analog input pin 0
+  Serial.println(sensorValue);  // prints the value read
+  delay(1000);                        // wait 100ms for next reading
+}
